11135cb779264790ba0352c2c2429d860b26a11c
Naimul Islam <naimulislam920@gmail.com>
2022-07-05 00:21:46 +0600

    update

--- resources/views/website/course/detail.blade.php
+++ resources/views/website/course/detail.blade.php
@@ -11,11 +11,13 @@
                         <img src="{{asset($course->image)}}" alt="" class="card-img-top" width="100" height="500">
                     </div>
                 </div>
-                <div class="col-md-8 mx-auto">
+                <div class="col-md-6 mx-auto">
                     <h1 class="text-center">{{$course->title}}</h1>
                     <hr>
-                    <p class="text-center">{{$course->short_description}}</p>
-                    <h3 class="text-center">Course Fee: Tk. {{$course->fee}}</h3>
+                    <p class="">{{$course->short_description}}</p>
+                    <h3 class="">Course Fee: Tk. {{$course->fee}}</h3>
+                    <hr>
+                    <a href="{{route('enroll.course')}}" class="btn btn-outline-info ">Enroll Course</a>
                 </div>
             </div>
         </div>
